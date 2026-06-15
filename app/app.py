8b1cc97bc1680289a44ebbe893efe0138cf890ae
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-06-15 13:49:51 -0300

    Feat: Criando banco de dados e configurando o app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,16 @@
-print("Hello World")
+from flask import Flask, render_template
+from app.database import engine, Base
+from app.models.usuario import Usuario
+from app.models.vaga import Vaga
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    with app.app_context():
+        Base.metadata.create_all(engine)
+        print("Banco de dados criado com sucesso!")
+    app.run(debug=True)
