c97df6824fe37cd46eae0b151c2903b9b5f85a20
JoaoVPalandi <j.palandi@aluno.senai.br>
2026-06-23 09:13:00 -0300

    Arrumando a pagina login e tentando resolver o conflito da API

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from app.database import engine, Base
-from flask_restx import Api
+# from flask_restx import Api
 from flask_cors import CORS
 
 import app.models.usuario
@@ -12,21 +12,24 @@ Base.metadata.create_all(bind=engine)
 app = Flask(__name__)
 CORS(app)
 
-api = Api(app,
-          title="TechMatch API",
-          description='API de Match de Vagas de Emprego',
-          version='1.0.0',
-          doc='/docs')
+# api = Api(app,
+#           title="TechMatch API",
+#           description='API de Match de Vagas de Emprego',
+#           version='1.0.0',
+#           doc='/docs')
 
+# @app.route('/')
+# def login():
+#     return render_template('login.html')
+    
 from app.route.usuario import usuario_bp
 from app.route.vagas import vagas_bp
+from app.route.login import login_bp
 
 app.register_blueprint(usuario_bp)
 app.register_blueprint(vagas_bp)
+app.register_blueprint(login_bp)
 
-@app.route('/')
-def login():
-    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
